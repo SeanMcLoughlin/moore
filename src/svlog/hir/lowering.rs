9364e4633a336013a7382ce57ea0956957fbf18a
Fabian Schuiki <fabian@schuiki.ch>
2020-11-05 16:09:17 +0100

    svlog: ast: Make CallArg a proper node

--- src/svlog/hir/lowering.rs
+++ src/svlog/hir/lowering.rs
@@ -1606,7 +1606,11 @@ fn lower_call<'a>(
         ast::SysIdentExpr(ident) => {
             let map_unary = || match args {
                 [ast::CallArg {
-                    expr: Some(ref arg),
+                    data:
+                        ast::CallArgData {
+                            expr: Some(ref arg),
+                            ..
+                        },
                     ..
                 }] => Ok(arg),
                 _ => {
@@ -1621,14 +1625,26 @@ fn lower_call<'a>(
                 || Ok(cx.map_ast_with_parent(AstNode::Expr(map_unary()?), expr.id()));
             let map_array_dim = |func| match args {
                 [ast::CallArg {
-                    expr: Some(ref arg),
+                    data:
+                        ast::CallArgData {
+                            expr: Some(ref arg),
+                            ..
+                        },
                     ..
                 }] => Ok(hir::BuiltinCall::ArrayDim(func, arg, None)),
                 [ast::CallArg {
-                    expr: Some(ref arg),
+                    data:
+                        ast::CallArgData {
+                            expr: Some(ref arg),
+                            ..
+                        },
                     ..
                 }, ast::CallArg {
-                    expr: Some(ref dim),
+                    data:
+                        ast::CallArgData {
+                            expr: Some(ref dim),
+                            ..
+                        },
                     ..
                 }] => Ok(hir::BuiltinCall::ArrayDim(func, arg, Some(dim))),
                 _ => {
@@ -1733,7 +1749,7 @@ fn lower_call_arg<'gcx>(
 ) -> hir::CallArg {
     hir::CallArg {
         span: ast.span,
-        name: ast.name.map(|n| Spanned::new(n, ast.name_span)),
+        name: ast.name,
         expr: ast
             .expr
             .as_ref()
